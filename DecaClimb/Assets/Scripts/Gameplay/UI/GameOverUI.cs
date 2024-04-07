f1fbc0cf3cfb8cf78f0eb4b0c6ec12c422cc8217
Aditya E Ajith <adityaeajith@gmail.com>
2024-04-08 05:03:55 +0530

    Created GameOverUI script
    - Added Text mesh pro

--- DecaClimb/Assets/Scripts/Gameplay/UI/GameOverUI.cs
+++ DecaClimb/Assets/Scripts/Gameplay/UI/GameOverUI.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Revity.DecaClimb
 {
+    /// <summary>
+    /// Controls the GmaeOver UI
+    /// </summary>
     public class GameOverUI : MonoBehaviour
     {
-        //[SerializeField] private 
-        public void InitializeGameOverUI()
+        [SerializeField] private GameObject GameOverPanel;
+        [SerializeField] private Button m_YesButton;
+        [SerializeField] private Image m_VideoButton;
+        [SerializeField] private Button m_NoButton;
+        [SerializeField] private TextMeshProUGUI m_HighscoreText;
+
+        public void Initialize()
         {
-            // set Yes button fi retry possible
+            m_YesButton.onClick.AddListener(null);
+            m_NoButton.onClick.AddListener(null);
+        }
+
+        /// <summary>
+        /// Initialize Game over UI
+        /// </summary>
+        /// <param name="isRetryAvailable">Based on this retry button is interactable</param>
+        /// <param name="highscore">To Displays highscore at the end</param>
+        public void ShowGameOverUI(bool isRetryAvailable, int highscore)
+        {
+            m_YesButton.interactable = isRetryAvailable;
+            m_VideoButton.gameObject.SetActive(isRetryAvailable);
             // Set Highscore
+
+            m_HighscoreText.text = $"Highscore: {highscore}";
+            GameOverPanel.SetActive(isRetryAvailable);
         }
     }
 }
