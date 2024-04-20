c87c1fc5da131959e6380f7afc255f1b758ae88b
Aditya E Ajith <adityaeajith@gmail.com>
2024-04-20 14:57:40 +0530

    added controlled coin spawning

--- DecaClimb/Assets/_Project/Scripts/Managers/PillarSpwan.cs
+++ DecaClimb/Assets/_Project/Scripts/Managers/PillarSpwan.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -5,80 +6,86 @@ namespace Revity.DecaClimb.Game
 {
     public class PillarSpwan : MonoBehaviour
     {
-        public GameObject coinSpawnner;
+		private int m_MinPillarCount;
+        private int m_MaxPillarCount;
 
-        Pillar m_pillar;
+        private List<Pillar> m_PillarList;
 
-        private int minPillar;
-        private int maxPillar;
+		private void Awake()
+		{
+			m_PillarList = new List<Pillar>();
+		}
 
 		public void NewLevel()
 		{
+            m_PillarList.Clear();
             transform.eulerAngles = Vector3.zero;
             Initialize();
 		}
 
 		public void Initialize()
 		{
+			SetPillarCountRange();
+			SpwanPillars();
+			SpawnCoins();
+		}
+
+		private void SetPillarCountRange()
+		{
 			int level = GameSceneService.Instance.LevelManager.CurrentLevel;
 			if (level == 0)
 			{
-				minPillar = 4;
-				maxPillar = 5;
+				m_MinPillarCount = 4;
+				m_MaxPillarCount = 5;
 			}
 			else if (level > 10)
 			{
-				minPillar = level;
-				maxPillar = level + 3;
+				m_MinPillarCount = level;
+				m_MaxPillarCount = level + 3;
 			}
 			else
 			{
-				minPillar = 7;
-				maxPillar = 10;
+				m_MinPillarCount = 7;
+				m_MaxPillarCount = 10;
 			}
-
-			SpwanPillars();
 		}
 
 		private void SpwanPillars()
         {
-            int pillarAmount = Random.Range(minPillar, maxPillar);
+            int pillarAmount = Random.Range(m_MinPillarCount, m_MaxPillarCount);
+			Pillar newPillar;
 
-            for (int i = 0; i <= pillarAmount; i++)
+			for (int i = 0; i <= pillarAmount; i++)
             {
-                m_pillar = GameSceneService.Instance.FactoryService.GetPillar();
-                m_pillar.PillarLevel = i;
-                m_pillar.transform.parent = transform;
-                m_pillar.transform.eulerAngles = Vector3.zero;
-                m_pillar.transform.position = new Vector3(transform.position.x, i * 4, transform.position.z);
-                SpwanCoinSpwaner(i);
+                newPillar = GameSceneService.Instance.FactoryService.GetPillar();
+                newPillar.PillarLevel = i;
+                newPillar.transform.parent = transform;
+                newPillar.transform.eulerAngles = Vector3.zero;
+                newPillar.transform.position = new Vector3(transform.position.x, i * 4, transform.position.z);
 
-                m_pillar.GetComponent<GroundManager>().Initialize(i == 0, i == pillarAmount);
+                newPillar.GetComponent<GroundManager>().Initialize(i == 0, i == pillarAmount);
+                m_PillarList.Add(newPillar);
             }
-
-            //pillars = GameSceneService.Instance.FactoryService.GetPillar().gameObject;
-            //lastpillar = pillarAmount * 4;
-            ProgressBarScript.StartProgress();
-            //pillars.transform.position = new Vector3(transform.position.x, lastpillar, transform.position.z);
-            //pillars.transform.parent = transform;
-
-			//pillars.GetComponent<GroundSpwanner>().Initialize(false, true);
 		}
 
-        private void SpwanCoinSpwaner(int i)
-        {
-            int isCoin = Random.Range(0, 2);
+		private void SpawnCoins()
+		{
+			// every level needs atleast 2 coins 
+			// -1 : exclude goal pillar
+			int coinCount = Random.Range(2, m_PillarList.Count - 1); 
 
-            if (isCoin == 1)
-            {
-                Coin coin = GameSceneService.Instance.FactoryService.GetCoin();
-                coin.transform.parent = m_pillar.transform;
-                coin.transform.position = m_pillar.transform.position;
-                coin.transform.rotation = Quaternion.Euler(0, Random.Range(i == 0 ? 20 : 0, i == 0 ? 330 : 360), 0);
-				//Instantiate(coinSpawnner, pillars.transform.position, Quaternion.identity, transform);
+			for (int i = 0; i < coinCount; i++)
+			{
+				Coin coin = GameSceneService.Instance.FactoryService.GetCoin();
 
+				int pillarIndex = Random.Range(0, m_PillarList.Count - 1); 
+				coin.transform.parent = m_PillarList[pillarIndex].transform;
+				
+				// Don't spawn right in front of player spawn
+				// player always spawn in 0th pillar with y rotation zero.
+				coin.transform.SetPositionAndRotation(m_PillarList[pillarIndex].transform.position, Quaternion.Euler(0, Random.Range(pillarIndex == 0 ? 20 : 0, pillarIndex == 0 ? 330 : 360), 0));
 			}
-        }
+		}
 
 	}
 }
